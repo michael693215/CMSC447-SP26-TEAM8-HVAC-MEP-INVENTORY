4fc384bc318ac0fc4d033b16f34662ab68506a07
ehale2 <ehale2@umbc.edu>
2026-04-21 13:42:16 -0400

    Bypass login

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -48,9 +48,10 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith('/auth')
   ) {
     // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+    //BYPASS Login
+    // const url = request.nextUrl.clone()
+    // url.pathname = '/login'
+    // return NextResponse.redirect(url)
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
